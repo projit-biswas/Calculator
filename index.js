05fb9f3753239cce1885415215246c7b0ef98f13
projitbiswas <122886202+projitbiswas@users.noreply.github.com>
2023-08-21 17:34:34 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+let input = document.getElementById("inputBox");
+let btn = document.querySelectorAll("button");
+let str = "";
+let arr = Array.from(btn);
+
+arr.forEach((btn) => {
+  btn.addEventListener("click", (e) => {
+    if (e.target.innerHTML == "=") {
+      str = eval(str);
+      input.value = str;
+    } else if (e.target.innerHTML == "AC") {
+      str = "";
+      input.value = str;
+    } else if (e.target.innerHTML == "DEL") {
+      str = str.substring(0, str.length - 1);
+      input.value = str;
+    } else {
+      str += e.target.innerHTML;
+      input.value = str;
+    }
+  });
+});
